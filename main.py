c142ce8c67dfb7de92cf5bcee6a96be238447006
jedrzej-gorski <jedrzej.gorski@student.put.poznan.pl>
2021-11-28 00:52:56 +0100

    Implementacja i trening UMM. Plik z indeksem tagów.

--- main.py
+++ main.py
@@ -2,55 +2,60 @@ import numpy as np
 
 class HiddenMarkovModel:
     def __init__(self):
-        self.transition_list = np.zeros((45, 45))                           # Lista przejść stanów - liczba_przejść_do_stanu0, liczba_przejść_do_stanu1, ..., liczba_przejść_do_stanu44}
-        self.emission_dict = {}                                             # Słownik z wyrażeniami - wyrażenie: [liczba_wystąpień_stan0, liczba_wystąpień_stan1, ... , liczba_wystąpień_stan44]
+        self.transition_list = np.zeros((45, 45))                           # Macierz licząca przejść stanów - liczba_przejść_do_stanu0, liczba_przejść_do_stanu1, ..., liczba_przejść_do_stanu44}                                       #
+        self.emission_list = [{} for x in range(0, 45)]                     # Lista słowników z wyrażeniami dla każdego stanu - {wyrażenie0_1: liczba_wystąpień_wyrażenie1, wyrażenie0_2: liczba_wystąpień_wyrażenie2, ... , wyrażenie0_x: liczba_wystąpień_wyrażeniex},
+                                                                            # {wyrażenie1_1: liczba wystąpień wyrażenie1, ..., wyrażenie1_x: liczba wystąpień_wyrażeniex}, ... {wyrażenie44_1: liczba_wystąpień_wyrażenie1, ..., wyrażenie44_x: liczba_wystąpień_wyrażeniex}
         self.tag_dict = {}                                                  # Słownik części mowy - część_mowy: nr_indeksu
+
+        self.transition_prob = np.zeros((45, 45))                           # Struktury zawierające prawdopodobieństwa przejścia stanów i emisji obserwacji.
+        self.emission_prob = [{} for x in range(0, 45)]
 
     def initialize_tags(self, tag_lines):                                   # Funkcja przypisująca częściom mowy w tag_dict indeksy 0-44.
         for line in tag_lines:
             tag_data = line.split()
             self.tag_dict[tag_data[1]] = int(tag_data[0])
-    def initialize_dicts(self, input_lines):
+    def initialize_lists(self, input_lines):
         previous_state = 0
         for line in input_lines:
             if line != "\n":
                 data = line.split()
-                expression = data[0]                                        # Przetwarzane wyrażenie...
+                expression = data[0].lower()                                # Przetwarzane wyrażenie...
                 tag_index = self.tag_dict[data[1]]                          # ...i odpowiadająca mu część mowy.
-                if expression in self.emission_dict:
-                    self.emission_dict[expression][tag_index] += 1          # Jeżeli wyrażenie jest zawarte w słowniku, dodaj 1 na pozycji odpowiadającej części mowy, jaką jest wyrażenie...
+                if expression in self.emission_list[tag_index]:
+                    self.emission_list[tag_index][expression] += 1          # Jeżeli wyrażenie jest zawarte w słowniku, dodaj 1 do wartości definicji...
                 else:
-                    self.emission_dict[expression] = [0] * 45               # ...a jeśli nie, to stwórz wpis w słowniku odpowiadający wyrażeniu, i zainicjalizuj listę 0, poza jedną 1 odpowiadającą części mowy, jaką jest wyrażenie.
-                    self.emission_dict[expression][tag_index] = 1
+                    self.emission_list[tag_index][expression] = 1           # ...a jeśli nie, to stwórz wpis w słowniku odpowiadający wyrażeniu, i ustaw definicję na 1.
                 self.transition_list[previous_state][tag_index] += 1        # Zaaktualizuj listę przejść, dodając 1 na indeksie [i][j], gdzie i to indeks stanu poprzedniego, a j to indeks stanu obecnego
                 previous_state = tag_index
             else:
                 previous_state = 0
-    def print_training_data(self):
-        for line in self.transition_list:
-            print(line)
-        for line in self.emission_dict:
-            print(line)
-
-
-
-
+    def initialize_probabilities(self):
+        for index, line in enumerate(self.transition_list):
+            incidence_sum = np.sum(line)                                    # Suma wszystkich przejść stanu.
+            if incidence_sum == 0:                                          # Jeżeli stan nie ma przejść, bo nie występuje, to wypełnij wiersz zerami.
+                new_row = [0] * 45
+            else:
+                new_row = np.array([value / incidence_sum for value in line])   # Lista ilorazów przejść do stanów 0,1,2,...,44 i sumy wszystkich przejść stanów.
+            self.transition_prob[index] = new_row
+        for index, line in enumerate(self.emission_list):
+            incidence_sum = sum([value for key, value in line.items()])             # Suma wszystkich obserwacji (wyrażeń) danego stanu.
+            if incidence_sum == 0:
+                new_row = {}                                                # Jeżeli stan nie ma obserwacji, bo nie występuje, to wstaw pusty słownik.
+            else:
+                new_row = {key: value / incidence_sum for key, value in line.items()}
+            self.emission_prob[index] = new_row
+    def initialize(self, input_lines, tag_lines):
+        self.initialize_tags(tag_lines)
+        self.initialize_lists(input_lines)
+        self.initialize_probabilities()
 
 def main():
     with open("train_input.txt") as input_file, open("taglist.txt") as taglist:
         input_lines = input_file.readlines()
         taglist_lines = taglist.readlines()
         hmm = HiddenMarkovModel()
-        hmm.initialize_tags(taglist_lines)
-        hmm.initialize_dicts(input_lines)
-        hmm.print_training_data()
+        hmm.initialize(input_lines, taglist_lines)
+        print("haha! :)")
 
-    # Use a breakpoint in the code line below to debug your script.
-    # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
